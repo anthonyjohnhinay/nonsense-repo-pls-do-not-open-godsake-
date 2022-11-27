e99cc42d56f5404f17d9cea34ffa287e19b18fd1
johnyjohny <74842720+johnyjohny20@users.noreply.github.com>
2022-11-27 22:10:36 +0800

    Update coin_adder.py

--- coin_adder.py
+++ coin_adder.py
@@ -2,13 +2,14 @@
 """
 Dynamic adding of coins amount and quantity 2d array
 """
+
 arr = []
 total = 0
 
 lim = int(input('enter a number: '))
 for i in range(lim):
     num = (input(f'enter num {i + 1}: '))
-    arr.append([int(x) for x in num.split()])
+    arr.append([float(x) for x in num.split()])
     
 for x in arr:
     total += x[0] * x[1]
